f02bcef605179f1eade3bb22531d278290c41a22
Burak Alış <burakalis5@gmail.com>
2022-08-20 12:16:41 +0300

    lesson 10

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 const createUser = async (req, res) => {
   try {
@@ -15,4 +16,40 @@ const createUser = async (req, res) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    console.log(req.body);
+
+    const user = await User.findOne({ username });
+
+    let same = false;
+
+    if (user) {
+      same = await bcrypt.compare(password, user.password);
+      console.log("same", same);
+    } else {
+      return res.status(401).json({
+        succeded: false,
+        err: "Kullanıcı Bulunamadı!",
+      });
+    }
+
+    if (same) {
+      res.status(200).send("Giriş Yaptınız!");
+    } else {
+      res.status(401).json({
+        succeded: false,
+        err: "Şifre yanlış!",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      succeded: false,
+      err,
+    });
+  }
+};
+
+export { createUser, loginUser };
