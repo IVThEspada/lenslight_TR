80100ba2548a410dde19cbd0a260ed59f74148a4
Burak Alış <burakalis5@gmail.com>
2022-08-24 11:07:58 +0300

    lesson 13

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,10 +5,7 @@ import Jwt from "jsonwebtoken";
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      succeded: true,
-      user,
-    });
+    res.redirect("/login");
   } catch (err) {
     res.status(500).json({
       succeded: false,
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -11,4 +11,6 @@ router.route("/register").get(pageController.getRegisterPage);
 
 router.route("/login").get(pageController.getLoginPage);
 
+router.route("/logout").get(pageController.getLogout);
+
 export default router;
